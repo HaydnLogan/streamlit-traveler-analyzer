0208223aa575bd495d830f266086e40c19da8e38
HaydnLogan <84303368+HaydnLogan@users.noreply.github.com>
2025-07-18 05:33:00 -0400

    Update TestB_app.py

--- TestB_app.py
+++ TestB_app.py
@@ -7,7 +7,7 @@ from models.models_a import run_a_model_detection
 from models.TestB_Mod_B_01 import run_b_model_detection
 from models.TestB_Mod_C_01 import run_c_model_detection
 
-# *** This is Tester B  ***
+# *** This is Tester B  🔬 ***
 
 # 🔌 Streamlit UI
 st.set_page_config(layout="wide")
