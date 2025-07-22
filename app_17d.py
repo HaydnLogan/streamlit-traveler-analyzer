9f83b9ddeea269b04e7022cd494206d3e6f0d8ae
HaydnLogan <84303368+HaydnLogan@users.noreply.github.com>
2025-07-22 13:48:25 -0400

    Update app_17d.py

--- app_17d.py
+++ app_17d.py
@@ -10,7 +10,7 @@ import io
 from pandas import ExcelWriter
 
 from shared.shared import clean_timestamp, process_feed, get_input_value, highlight_anchor_origins
-from models.models_a_02g1 import run_a_model_detection
+from models.models_a_02r1 import run_a_model_detection
 from models.mod_b_05pg1 import run_b_model_detection
 from models.mod_c_03gp import run_c_model_detection
 from models.mod_x_03g import run_x_model_detection
